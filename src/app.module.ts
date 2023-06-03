e936a10aaddc9895241416c3e60ea717089c151a
joannabiernat <j.biernat955@gmail.com>
2023-06-03 20:47:15 +0200

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { MoviesModule } from "./movies/movies.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmConfig } from "./typeorm.config";
 import { UserModule } from "./user/user.module";
+import { MovieEntity } from "./movies/entities/movie.entity";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), MoviesModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([MovieEntity]),
+    MoviesModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { MovieEntity } from "./movies/entities/movie.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  constructor(
+    @InjectRepository(MovieEntity)
+    private readonly movieRepository: Repository<MovieEntity>,
+  ) {}
+  async getMoviesSample(): Promise<MovieEntity[]> {
+    return this.movieRepository
+      .createQueryBuilder()
+      .select()
+      .orderBy("RAND()")
+      .limit(10)
+      .getMany();
   }
 }
