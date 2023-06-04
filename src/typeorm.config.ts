2ee706359c5cc4a5721468e0c0fa731ce2e53b0a
joannabiernat <j.biernat955@gmail.com>
2023-06-04 16:47:12 +0200

    commit

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "mysql",
-  host: "localhost",
-  port: 8889,
-  username: "root",
-  password: "root",
-  database: "showflix",
+  host: process.env.DB_HOST || "localhost",
+  port: +process.env.DB_PORT || 8889,
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "showflix",
   entities: [__dirname + "/**/*.entity{.ts,.js}"],
-  synchronize: true,
-  dropSchema: true,
+  synchronize: process.env.DB_SYNCHRONIZE === "true" || true,
+  dropSchema: process.env.DB_DROP_SCHEMA === "true" || true,
 };
