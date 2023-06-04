c9f5d6683cb871b8cbbc9b050d535360ee4aa099
joannabiernat <j.biernat955@gmail.com>
2023-06-04 17:48:46 +0200

    commit

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,15 +1,49 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+//
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: "mysql",
+//   host: process.env.DB_HOST || "localhost",
+//   port: +process.env.DB_PORT || 8889,
+//   username: process.env.DB_USERNAME || "root",
+//   password: process.env.DB_PASSWORD || "root",
+//   database: process.env.DB_NAME || "showflix",
+//   entities: [__dirname + "/**/*.entity{.ts,.js}"],
+//   synchronize: Boolean(process.env.DB_SYNCHRONIZE) || true,
+//   dropSchema: Boolean(process.env.DB_DROP_SCHEMA) || true,
+// };
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: +process.env.DB_PORT || 8889,
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "showflix",
-  entities: [__dirname + "/**/*.entity{.ts,.js}"],
-  synchronize: Boolean(process.env.DB_SYNCHRONIZE) || true,
-  dropSchema: Boolean(process.env.DB_DROP_SCHEMA) || true,
-};
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as url from "url";
+
+const jawsdbUrl = process.env.JAWSDB_URL;
+
+let typeOrmConfig: TypeOrmModuleOptions;
+if (jawsdbUrl) {
+  const connectionUrl = new url.URL(jawsdbUrl);
+
+  typeOrmConfig = {
+    type: "mysql",
+    host: connectionUrl.hostname,
+    port: Number(connectionUrl.port),
+    username: connectionUrl.username,
+    password: connectionUrl.password,
+    database: connectionUrl.pathname.substr(1),
+    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    synchronize: true,
+  };
+} else {
+  typeOrmConfig = {
+    type: "mysql",
+    host: "localhost",
+    port: 8889,
+    username: "root",
+    password: "root",
+    database: "showflix",
+    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    synchronize: true,
+  };
+}
+
+export default typeOrmConfig;
